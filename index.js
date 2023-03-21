df24d051c22742c0b2eab75ae44480c566acfe5b
Swarup Ahmed <swarup692@gmail.com>
2023-03-22 00:45:55 +0600

    second commit

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(serveStatic(path.join(__dirname, 'public')));
+if(!fs.existsSync('/Uploads')){
+    fs.mkdir('/Uploads')
+}
 app.use(serveStatic(path.join(__dirname, 'Uploads')));
 
 app.get('/Identity/Account/Projects', async (req, res) => {
